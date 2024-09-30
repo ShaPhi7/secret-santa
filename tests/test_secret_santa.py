d94b447e7900f27f1bfaba609f7c9aaad2ee4d89
Shaun Phillips <smphillips@alumni.york.ac.uk>
2024-09-30 12:16:08 +0100

    Add test for valid count

--- tests/test_secret_santa.py
+++ tests/test_secret_santa.py
@@ -4,19 +4,38 @@ import secret_santa
 class TestSecretSanta(unittest.TestCase):
 
     def setUp(self):
-        self.people = ["A1", "A2", "B1", "B2", "C1", "C2", "C3"]
+        self.people = ["A1", "A2", "B1"]
 
     def test_generate_permutations(self):
+        people = ["A1", "A2", "B1"]
+        perms = secret_santa.generate_permutations(people)
+        expected_perms = [
+            ('A1', 'A2', 'B1'),
+            ('A1', 'B1', 'A2'),
+            ('A2', 'A1', 'B1'),
+            ('A2', 'B1', 'A1'),
+            ('B1', 'A1', 'A2'),
+            ('B1', 'A2', 'A1')
+        ]
+        self.assertEqual(perms, expected_perms)
+
+    def test_generate_permutations_count(self):
+        self.people = ["A1", "A2", "B1", "B2", "C1", "C2", "C3"]
         permutations = secret_santa.generate_permutations(self.people)
         self.assertEqual(len(permutations), 5040) #7!
 
     def test_valid_draw(self):
-        draw = ["A2", "B1", "B2", "C1", "C2", "C3", "A1"]
+        draw = ["A2", "B1", "A1"]
         self.assertTrue(secret_santa.valid(self.people, draw))
 
     def test_drawing_self_is_invalid(self):
-        draw = ["A1", "A2", "B1", "B2", "C1", "C2", "C3"]
+        draw = ["A1", "B1", "A2"]
         self.assertFalse(secret_santa.valid(self.people, draw))
+
+    def test_count_valid_permutations(self):
+        permutations = secret_santa.generate_permutations(self.people)
+        count = secret_santa.countValidPermutations(self.people, permutations)
+        self.assertEqual(count, 2) # ('A1', 'A2', 'B1') and ('A2', 'A1', 'B1') (they have original positions)
 
 if __name__ == '__main__':
     unittest.main()
