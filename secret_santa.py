e72569a4507254a777bdf4ecf7ad643ead10b477
Shaun Phillips <smphillips@alumni.york.ac.uk>
2024-09-30 11:33:49 +0100

    Add valid method

--- secret_santa.py
+++ secret_santa.py
@@ -3,6 +3,11 @@ import itertools
 def generate_permutations(people):
     return list(itertools.permutations(people))
 
+def valid(people, draw):
+    people = draw
+    draw = people
+    return True
+
 def main():
     permutations = generate_permutations(["A1", "A2", "B1", "B2", "C1", "C2", "C3"])
     print(permutations)
--- tests/test_secret_santa.py
+++ tests/test_secret_santa.py
@@ -1,5 +1,5 @@
 import unittest
-from secret_santa import generate_permutations
+import secret_santa
 
 class TestSecretSanta(unittest.TestCase):
 
@@ -7,16 +7,16 @@ class TestSecretSanta(unittest.TestCase):
         self.people = ["A1", "A2", "B1", "B2", "C1", "C2", "C3"]
 
     def test_generate_permutations(self):
-        permutations = generate_permutations(self.people)
+        permutations = secret_santa.generate_permutations(self.people)
         self.assertEqual(len(permutations), 5040) #7!
 
     def test_valid_draw(self):
         draw = ["A2", "B1", "B2", "C1", "C2", "C3", "A1"]
-        self.assertTrue(valid(self.people, draw))
+        self.assertTrue(secret_santa.valid(self.people, draw))
 
     def test_drawing_self_is_invalid(self):
         draw = ["A1", "A2", "B1", "B2", "C1", "C2", "C3"]
-        self.assertFalse(valid(self.people, draw))
+        self.assertFalse(secret_santa.valid(self.people, draw))
 
 if __name__ == '__main__':
     unittest.main()
